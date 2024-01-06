68980be5e750598c3123c0e4f170a5cf58027d85
Nathaniel Capule <nmcapule@gmail.com>
2024-01-06 18:50:26 +0800

    Use pixijs for rendering instead of plain old HTML canvas.

--- src/components/flow-fields/flow-fields.tsx
+++ src/components/flow-fields/flow-fields.tsx
@@ -1,4 +1,5 @@
 import { Component, h } from '@stencil/core';
+import * as PIXI from 'pixi.js';
 
 class Vec2 {
   constructor(readonly x: number, readonly y: number) {}
@@ -31,67 +32,58 @@ const COLOR_1 = '#D2E3C8';
 })
 export class FlowFields {
   canvas!: HTMLCanvasElement;
+  app!: PIXI.Application;
   grid: Array<Array<Vec2>>;
+
+  cellSize = 16;
 
   componentDidLoad() {
     // Set internal resolution of canvas to match it's DOM size.
     this.canvas.width = this.canvas.offsetWidth;
     this.canvas.height = this.canvas.offsetHeight;
 
-    const ctx = this.canvas.getContext('2d');
+    this.app = new PIXI.Application({
+      view: this.canvas,
+      resizeTo: this.canvas,
+    });
 
-    ctx.fillStyle = COLOR_1;
-    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-    const cellSize = 8;
     this.grid = [];
 
-    for (let col = 0; col * cellSize < ctx.canvas.width; col++) {
+    for (let col = 0; col * this.cellSize < this.canvas.width; col++) {
       this.grid.push([]);
-      for (let row = 0; row * cellSize < ctx.canvas.height; row++) {
+      for (let row = 0; row * this.cellSize < this.canvas.height; row++) {
+        const angle = (Math.PI * col * (row * this.cellSize)) / this.canvas.height;
         this.grid[col].push(
-          Vec2.of(Math.cos(Math.PI * 2 * (col / ctx.canvas.width)), Math.cos(Math.PI * 3))
-            .normalize()
-            .multiply(cellSize / 4),
+          Vec2.of(Math.cos(angle), Math.sin(angle)).normalize().multiply(this.cellSize),
         );
-
-        const x = col * cellSize;
-        const y = row * cellSize;
-        const midX = x + cellSize / 2;
-        const midY = y + cellSize / 2;
-
-        // Draw checkboard.
-        if ((col + row) % 2 === 0) {
-          ctx.fillStyle = COLOR_2;
-          ctx.fillRect(x, y, cellSize, cellSize);
-        }
-
-        // Draw vector.
-        ctx.strokeStyle = COLOR_4;
-        ctx.beginPath();
-        ctx.moveTo(midX, midY);
-        ctx.lineTo(midX + this.grid[col][row].x, midY + this.grid[col][row].y);
-        ctx.stroke();
       }
     }
 
-    const steps = 50;
-    for (let i = 0; i < 500; i++) {
+    this.drawFlowFields();
+  }
+
+  private drawFlowFields() {
+    const colors = [COLOR_4, COLOR_3, COLOR_2, COLOR_1, 'white', 'red', 'blue', 'green'];
+
+    const steps = 5;
+    for (let i = 0; i < 3000; i++) {
+      const graphics = new PIXI.Graphics();
       let curX = Math.random() * this.canvas.width;
       let curY = Math.random() * this.canvas.height;
-      ctx.strokeStyle = COLOR_4;
-      ctx.moveTo(curX, curY);
+      graphics.lineStyle(1, colors[Math.floor(Math.random() * colors.length)], 1);
+      graphics.moveTo(curX, curY);
       for (let j = 0; j < steps; j++) {
-        const cell = this.grid[Math.floor(curX / cellSize)]?.[Math.floor(curY / cellSize)];
+        const cell =
+          this.grid[Math.floor(curX / this.cellSize)]?.[Math.floor(curY / this.cellSize)];
         if (!cell) continue;
 
         const nextX = curX + cell.x;
         const nextY = curY + cell.y;
-        ctx.lineTo(nextX, nextY);
+        graphics.lineTo(nextX, nextY);
         curX = nextX;
         curY = nextY;
       }
-      ctx.stroke();
+      this.app.stage.addChild(graphics);
     }
   }
 
